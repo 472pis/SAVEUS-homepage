de85f6927cf2ee68473816daa05939565853a89f
472pis <57526098+472pis@users.noreply.github.com>
2022-11-23 23:03:21 +0900

    onload -> addeventlistener

--- js/topEvent.js
+++ js/topEvent.js
@@ -8,11 +8,13 @@ let selectBox = document.getElementsByClassName("selectbox")[0];
 var info = document.getElementsByClassName("char_info");
 
 //로딩 후 이벤트 리스너 실행
-window.onload = function(){
+window.addEventListener('DOMContentLoaded', function()
+{
     for(var i=0; i<charBt.length; i++){
         charBt[i].addEventListener('click', clickChip);
     }
-};
+});
+
 
 function clickChip(e){
     var thisId = Number(this.id);
